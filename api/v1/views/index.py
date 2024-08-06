51b3ae8c2ff09307a262fff43623b5fdd2a77b20
JosepharDev <josephardev@gmail.com>
2024-08-06 03:14:03 -0700

    fix status endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,6 +12,12 @@ from models.state import State
 from models.user import User
 
 
+@app_views.route('/status')
+def get_status():
+    '''get status of api'''
+    return jsonify(status="OK")
+
+
 @app_views.route('/stats')
 def get_stats():
     ''' get stats of api'''
